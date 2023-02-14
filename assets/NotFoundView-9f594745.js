0423abe7ec3e42c5744de32ab7d8c08f2e8832b1
eric <ana31005@gmail.com>
2023-02-14 23:01:08 +0800

    deploy

--- assets/NotFoundView-9f594745.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c}from"./index-807d51c3.js";const n={};function t(r,s){return o(),c("div",null,"NotFound")}const a=e(n,[["render",t]]);export{a as default};
---
+++ assets/NotFoundView-e745809b.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-42d43df8.js";const n={};function t(r,s){return o(),c("div",null,"NotFound")}const a=e(n,[["render",t]]);export{a as default};

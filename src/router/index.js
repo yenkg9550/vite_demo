569a3b901df4ee33e2302f75285532c80f0ff4ea
eric <ana31005@gmail.com>
2023-02-14 23:07:20 +0800

    新增404頁面

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,10 @@ const router = createRouter({
         },
       ],
     },
+    {
+      path: "/:pathMatch(.*)*",
+      component: () => import("../views/NotFoundView.vue"),
+    },
   ],
 });
 
